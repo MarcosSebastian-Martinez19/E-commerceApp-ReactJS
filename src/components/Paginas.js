a36b0a99a5600fb4b79f124bef728e249d7df2b8
Marcos Martínez <marcos@mx>
2023-02-24 18:50:03 -0300

    Actualización de las Rutas

--- src/components/Paginas.js
+++ src/components/Paginas.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import { Inicio } from './Inicio/Inicio';
 import { Productos } from './Productos/Productos';
+import { ProductoDetalles } from './Productos/ProductoDetales/ProductoDetalles';
 
 export const Paginas = () => {
   return (
@@ -9,6 +10,7 @@ export const Paginas = () => {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/productos" element={<Productos />} />
+        <Route path="/producto/:id" element={<ProductoDetalles />} />
       </Routes>
     </section>
   )
